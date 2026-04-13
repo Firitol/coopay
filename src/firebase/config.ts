d98c1766bc4f7c4451af4613d8f2cedb062794cc
Firitol Erena <Firittoo@gmail.com>
2026-04-13 19:48:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3319063405-4e1d0",
+  "appId": "1:65882197969:web:0d22eab79899c156f5daf0",
+  "apiKey": "AIzaSyDhbcFbGIM9NTG70E7Zz_iZ8VFWX-hxsjk",
+  "authDomain": "studio-3319063405-4e1d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "65882197969"
+};
